3a458beaee39d54c7e5a156edac5e293173f2aba
Kiran-Aajage <kiranaajage09@gmail.com>
2025-03-27 15:47:43 +0100

    Saved changes to RestaurantMenu.js before switching branches

--- React/src/Components/RestaurantMenu.js
+++ React/src/Components/RestaurantMenu.js
@@ -24,7 +24,7 @@ const RestaurantMenu = () => {
     // orgin 
 
     const fetchMenu = async () => {
-        const data = await fetch ( "https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=12.9352403&lng=77.624532&restaurantId=" +resId);
+        const data = await fetch ( "https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=12.9352403&lng=77.624532&restaurantId=10582&catalog_qa=undefined&submitAction=ENTER");
         
         const json = await data.json();
         console.log(json);
